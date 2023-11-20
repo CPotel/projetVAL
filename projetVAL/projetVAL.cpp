c90a6e0b0ec96758f130369c77c1cfa2991f8c67
CPotel <cyriaque.potel@student.junia.com>
2023-11-20 10:11:32 +0100

    Ajoutez des fichiers projet.

---
+++ projetVAL/projetVAL.cpp
@@ -0,0 +1,12 @@
+﻿// projetVAL.cpp : définit le point d'entrée de l'application.
+//
+
+#include "projetVAL.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
