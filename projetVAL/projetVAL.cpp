4ac130f824331fe68b55d3d7d49b3f3be149b229
Julien Legrand <julien.legrand@student.junia.com>
2023-12-03 19:44:43 +0100

    Affichage des rails entre chaque stations

--- projetVAL/projetVAL.cpp
+++ projetVAL/projetVAL.cpp
@@ -193,5 +193,173 @@ int main()
 		}
 	);
 	
+	sf::RenderWindow window(sf::VideoMode(1500, 800), "Visualisation du métro Lillois");
 
+
+	sf::Texture TextureGare;
+	sf::Sprite Gare1, Gare2, Gare3, Gare4, Gare5, Gare6, Gare7, Gare8, Gare9, Gare10;
+	sf::RectangleShape Ligne1Gare1Gare2, Ligne1Gare2Gare3, Ligne1Gare3Gare4, Ligne1Gare4Gare5, Ligne1Gare5Gare6, Ligne1Gare6Gare7, Ligne1Gare7Gare8, Ligne1Gare8Gare9, Ligne1Gare9Gare10, Ligne2Gare1Gare2, Ligne2Gare2Gare3, Ligne2Gare3Gare4, Ligne2Gare4Gare5, Ligne2Gare5Gare6, Ligne2Gare6Gare7, Ligne2Gare7Gare8, Ligne2Gare8Gare9, Ligne2Gare9Gare10;
+	if (!TextureGare.loadFromFile(std::string("C:/Program Files/SFML/img/gare.png")))
+	{
+		cerr << "Erreur pendant le chargement des images" << endl;
+		//return EXIT_FAILURE;
+	}
+	//On mets à toutes les gares la même texture
+	Gare1.setTexture(TextureGare);
+	Gare2.setTexture(TextureGare);
+	Gare3.setTexture(TextureGare);
+	Gare4.setTexture(TextureGare);
+	Gare5.setTexture(TextureGare);
+	Gare6.setTexture(TextureGare);
+	Gare7.setTexture(TextureGare);
+	Gare8.setTexture(TextureGare);
+	Gare9.setTexture(TextureGare);
+	Gare10.setTexture(TextureGare);
+	//MaJ des positions et de la tailles des différentes gares
+	//Les gares :
+	Gare1.setPosition(sf::Vector2f(25, 0));
+	Gare1.setScale(sf::Vector2f(0.1, 0.1));
+	Gare2.setPosition(sf::Vector2f(125, 200));
+	Gare2.setScale(sf::Vector2f(0.1, 0.1));
+	Gare3.setPosition(sf::Vector2f(250, 500));
+	Gare3.setScale(sf::Vector2f(0.1, 0.1));
+	Gare4.setPosition(sf::Vector2f(450, 650));
+	Gare4.setScale(sf::Vector2f(0.1, 0.1));
+	Gare5.setPosition(sf::Vector2f(650, 650));
+	Gare5.setScale(sf::Vector2f(0.1, 0.1));
+	Gare6.setPosition(sf::Vector2f(800, 500));
+	Gare6.setScale(sf::Vector2f(0.1, 0.1));
+	Gare7.setPosition(sf::Vector2f(900, 300));
+	Gare7.setScale(sf::Vector2f(0.1, 0.1));
+	Gare8.setPosition(sf::Vector2f(1000, 150));
+	Gare8.setScale(sf::Vector2f(0.1, 0.1));
+	Gare9.setPosition(sf::Vector2f(1200, 250));
+	Gare9.setScale(sf::Vector2f(0.1, 0.1));
+	Gare10.setPosition(sf::Vector2f(1350, 450));
+	Gare10.setScale(sf::Vector2f(0.1, 0.1));
+	//gare 1 --> gare 2
+	Ligne1Gare1Gare2.setPosition(sf::Vector2f(50, 50));
+	Ligne1Gare1Gare2.setSize(sf::Vector2f(2, 195));
+	Ligne1Gare1Gare2.rotate(335);
+	Ligne1Gare1Gare2.setFillColor(sf::Color(0, 0, 0));
+	Ligne2Gare1Gare2.setPosition(sf::Vector2f(75, 50));
+	Ligne2Gare1Gare2.setSize(sf::Vector2f(2, 170));
+	Ligne2Gare1Gare2.rotate(335);
+	Ligne2Gare1Gare2.setFillColor(sf::Color(0, 0, 0));
+	//gare 2 --> gare 3
+	Ligne1Gare2Gare3.setPosition(sf::Vector2f(140, 250));
+	Ligne1Gare2Gare3.setSize(sf::Vector2f(2, 304));
+	Ligne1Gare2Gare3.rotate(338);
+	Ligne1Gare2Gare3.setFillColor(sf::Color(0, 0, 0));
+	Ligne2Gare2Gare3.setPosition(sf::Vector2f(165, 250));
+	Ligne2Gare2Gare3.setSize(sf::Vector2f(2, 275));
+	Ligne2Gare2Gare3.rotate(338);
+	Ligne2Gare2Gare3.setFillColor(sf::Color(0, 0, 0));
+	//gare 3 --> gare 4
+	Ligne1Gare3Gare4.setPosition(sf::Vector2f(260, 550));
+	Ligne1Gare3Gare4.setSize(sf::Vector2f(2, 233));
+	Ligne1Gare3Gare4.rotate(305);
+	Ligne1Gare3Gare4.setFillColor(sf::Color(0, 0, 0));
+	Ligne2Gare3Gare4.setPosition(sf::Vector2f(300, 550));
+	Ligne2Gare3Gare4.setSize(sf::Vector2f(2, 200));
+	Ligne2Gare3Gare4.rotate(305);
+	Ligne2Gare3Gare4.setFillColor(sf::Color(0, 0, 0));
+	//gare 4 --> gare 5
+	Ligne1Gare4Gare5.setPosition(sf::Vector2f(500, 690));
+	Ligne1Gare4Gare5.setSize(sf::Vector2f(2, 155));
+	Ligne1Gare4Gare5.rotate(270);
+	Ligne1Gare4Gare5.setFillColor(sf::Color(0, 0, 0));
+	Ligne2Gare4Gare5.setPosition(sf::Vector2f(485, 665));
+	Ligne2Gare4Gare5.setSize(sf::Vector2f(2, 182));
+	Ligne2Gare4Gare5.rotate(270);
+	Ligne2Gare4Gare5.setFillColor(sf::Color(0, 0, 0));
+	//gare 5 --> gare 6
+	Ligne1Gare5Gare6.setPosition(sf::Vector2f(700, 690));
+	Ligne1Gare5Gare6.setSize(sf::Vector2f(2, 200));
+	Ligne1Gare5Gare6.rotate(225);
+	Ligne1Gare5Gare6.setFillColor(sf::Color(0, 0, 0));
+	Ligne2Gare5Gare6.setPosition(sf::Vector2f(685, 665));
+	Ligne2Gare5Gare6.setSize(sf::Vector2f(2, 170));
+	Ligne2Gare5Gare6.rotate(225);
+	Ligne2Gare5Gare6.setFillColor(sf::Color(0, 0, 0));
+	//gare 6 --> gare 7
+	Ligne1Gare6Gare7.setPosition(sf::Vector2f(825, 500));
+	Ligne1Gare6Gare7.setSize(sf::Vector2f(2, 175));
+	Ligne1Gare6Gare7.rotate(210);
+	Ligne1Gare6Gare7.setFillColor(sf::Color(0, 0, 0));
+	Ligne2Gare6Gare7.setPosition(sf::Vector2f(840, 530));
+	Ligne2Gare6Gare7.setSize(sf::Vector2f(2, 210));
+	Ligne2Gare6Gare7.rotate(210);
+	Ligne2Gare6Gare7.setFillColor(sf::Color(0, 0, 0));
+	//gare 7 --> gare 8
+	Ligne1Gare7Gare8.setPosition(sf::Vector2f(930, 300));
+	Ligne1Gare7Gare8.setSize(sf::Vector2f(2, 120));
+	Ligne1Gare7Gare8.rotate(215);
+	Ligne1Gare7Gare8.setFillColor(sf::Color(0, 0, 0));
+	Ligne2Gare7Gare8.setPosition(sf::Vector2f(945, 325));
+	Ligne2Gare7Gare8.setSize(sf::Vector2f(2, 150));
+	Ligne2Gare7Gare8.rotate(215);
+	Ligne2Gare7Gare8.setFillColor(sf::Color(0, 0, 0));
+	//gare 8 --> gare 9
+	Ligne1Gare8Gare9.setPosition(sf::Vector2f(1030, 155));
+	Ligne1Gare8Gare9.setSize(sf::Vector2f(2, 210));
+	Ligne1Gare8Gare9.rotate(300);
+	Ligne1Gare8Gare9.setFillColor(sf::Color(0, 0, 0));
+	Ligne2Gare8Gare9.setPosition(sf::Vector2f(1050, 190));
+	Ligne2Gare8Gare9.setSize(sf::Vector2f(2, 180));
+	Ligne2Gare8Gare9.rotate(300);
+	Ligne2Gare8Gare9.setFillColor(sf::Color(0, 0, 0));
+	//gare 9 --> gare 10
+	Ligne1Gare9Gare10.setPosition(sf::Vector2f(1225, 300));
+	Ligne1Gare9Gare10.setSize(sf::Vector2f(2, 220));
+	Ligne1Gare9Gare10.rotate(320);
+	Ligne1Gare9Gare10.setFillColor(sf::Color(0, 0, 0));
+	Ligne2Gare9Gare10.setPosition(sf::Vector2f(1250, 300));
+	Ligne2Gare9Gare10.setSize(sf::Vector2f(2, 195));
+	Ligne2Gare9Gare10.rotate(320);
+	Ligne2Gare9Gare10.setFillColor(sf::Color(0, 0, 0));
+
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear(sf::Color(255, 255, 255));
+		//Affichage des gares et des lignes de metro
+
+		window.draw(Gare1);
+		window.draw(Gare2);
+		window.draw(Gare3);
+		window.draw(Gare4);
+		window.draw(Gare5);
+		window.draw(Gare6);
+		window.draw(Gare7);
+		window.draw(Gare8);
+		window.draw(Gare9);
+		window.draw(Gare10);
+		window.draw(Ligne1Gare1Gare2);
+		window.draw(Ligne1Gare2Gare3);
+		window.draw(Ligne1Gare3Gare4);
+		window.draw(Ligne1Gare4Gare5);
+		window.draw(Ligne1Gare5Gare6);
+		window.draw(Ligne1Gare6Gare7);
+		window.draw(Ligne1Gare7Gare8);
+		window.draw(Ligne1Gare8Gare9);
+		window.draw(Ligne1Gare9Gare10);
+		window.draw(Ligne2Gare1Gare2);
+		window.draw(Ligne2Gare2Gare3);
+		window.draw(Ligne2Gare3Gare4);
+		window.draw(Ligne2Gare4Gare5);
+		window.draw(Ligne2Gare5Gare6);
+		window.draw(Ligne2Gare6Gare7);
+		window.draw(Ligne2Gare7Gare8);
+		window.draw(Ligne2Gare8Gare9);
+		window.draw(Ligne2Gare9Gare10);
+		window.display();
+	}
 }
